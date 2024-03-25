1dc713f413ba9d247e4d45894c053a6bf707eecd
M Saqib <m43577535@gmail.com>
2024-03-25 19:14:32 +0500

    Adding Complete logic for Qurtz task scheduler

---
+++ QuartzNET/BackgroundJob.cs
@@ -0,0 +1,37 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzNET
+{
+    internal class BackgroundJob : IJob
+    {
+        //__________ Excecution _____________
+        //excecution is method in which we write our job..  which execute when task run
+        //ok got it
+        public async Task Execute(IJobExecutionContext context)
+        {
+            //________ Simple excecution ____
+            //await Console.Out.WriteLineAsync("Executing background job");
+
+            //________ Geting value form context ____
+            var jobdataMap = context.MergedJobDataMap;
+            var isActive = jobdataMap.GetBoolean("isActive");
+
+            if (isActive)
+            {
+                var OutPutMessage = jobdataMap.GetString("OutPutMessage");
+                await Console.Out.WriteLineAsync(OutPutMessage);
+            }
+            else
+            {
+                await Console.Out.WriteLineAsync("Pakistan zinedabad");
+            }
+
+            
+        }
+    }
+}
--- QuartzNET/Program.cs
+++ QuartzNET/Program.cs
@@ -1,2 +1,155 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Quartz;
+using QuartzNET;
+
+
+//===============================  Carts Quartz =====================================
+#region Infinit_ Task
+//var job = JobBuilder.Create<BackgroundJob>()
+//    .WithIdentity(name: "BackgroundJob", group: "JobGroup")
+//    .Build();
+
+//var trigger = TriggerBuilder.Create()
+//    .WithIdentity(name: "RepeatingTrigger", group: "TriggerGroup")
+//    .WithSimpleSchedule(o => o
+//        .RepeatForever()
+//        .WithIntervalInSeconds(5))
+//    .Build();
+
+//var schedulerFactory = SchedulerBuilder.Create().Build();
+//var scheduler = await schedulerFactory.GetScheduler();
+//await scheduler.Start();
+
+//await scheduler.ScheduleJob(job, trigger);
+//Console.ReadKey();
+
+
+
+
+
+
+//using System;
+//using System.Threading.Tasks;
+//using Quartz;
+//using Quartz.Impl;
+
+//class Program
+//{
+//    static async Task Main(string[] args)
+//    {
+//        var job = JobBuilder.Create<BackgroundJob>()
+//            .WithIdentity(name: "BackgroundJob", group: "JobGroup")
+//            .Build();
+
+//        var trigger = TriggerBuilder.Create()
+//            .WithIdentity(name: "RepeatingTrigger", group: "TriggerGroup")
+//            .WithSimpleSchedule(o => o
+//                .RepeatForever()
+//                .WithIntervalInSeconds(5))
+//            .Build();
+
+
+//        var schedulerFactory = new StdSchedulerFactory();
+//        var scheduler = await schedulerFactory.GetScheduler();
+//        await scheduler.Start();
+
+//        await scheduler.ScheduleJob(job, trigger);
+
+//        Console.WriteLine("Press any key to exit...");
+//        Console.ReadKey();
+//    }
+//}
+
+//public class BackgroundJob : IJob
+//{
+//    public async Task Execute(IJobExecutionContext context)
+//    {
+//        await Console.Out.WriteLineAsync("Executing background job");
+//    }
+//}
+
+
+#endregion
+
+//=============================== Passing data to Quartztask =====================================
+#region Passing_data_quartzLibarary
+
+//var job = JobBuilder.Create<BackgroundJob>()
+//    .WithIdentity(name: "BackgroundJob", group: "JobGroup")
+//    //_____ send value to excecuton method _____
+//    .UsingJobData("OutPutMessage", "Dill Dill Pakistan Jaan jan Pakistan")
+//    .UsingJobData("isActive", true)
+//    .Build();
+
+//var trigger = TriggerBuilder.Create()
+//    .WithIdentity(name: "RepeatingTrigger", group: "TriggerGroup")
+//    .WithSimpleSchedule(o => o
+//        .RepeatForever()
+//        .WithIntervalInSeconds(5))
+//    .Build();
+
+//var schedulerFactory = SchedulerBuilder.Create().Build();
+//var scheduler = await schedulerFactory.GetScheduler();
+//await scheduler.Start();
+
+//await scheduler.ScheduleJob(job, trigger);
+//Console.ReadKey();
+#endregion
+
+
+//=============================== Daily+time  to Quartztask =====================================
+#region Daily+time spacifice
+//var job = JobBuilder.Create<BackgroundJob>()
+//    .WithIdentity(name: "BackgroundJob", group: "JobGroup")
+//    //_____ send value to execution method _____
+//    .UsingJobData("OutPutMessage", "Dill Dill Pakistan Jaan jan Pakistan")
+//    .UsingJobData("isActive", true)
+//    .Build();
+
+//var trigger = TriggerBuilder.Create()
+//    .WithIdentity(name: "RepeatingTrigger", group: "TriggerGroup")
+//    .WithDailyTimeIntervalSchedule(s =>
+//        //s.WithIntervalInMinutes(20)
+//        s.WithIntervalInSeconds(2)
+//         .OnEveryDay()
+//         .StartingDailyAt(TimeOfDay.HourMinuteAndSecondOfDay(2, 40, 0)) //(9, 0, 0)) // Start at 9:00 am
+//         .EndingDailyAt(TimeOfDay.HourMinuteAndSecondOfDay(2, 40, 40)) //(22, 0, 0)) // End at 10:00 pm
+//    )
+//    .Build();
+
+//var schedulerFactory = SchedulerBuilder.Create().Build();
+//var scheduler = await schedulerFactory.GetScheduler();
+//await scheduler.Start();
+
+//await scheduler.ScheduleJob(job, trigger);
+//Console.ReadKey();
+
+#endregion
+
+
+//=============================== spacifice Day  to Quartztask =====================================
+#region spacificeDay
+var job = JobBuilder.Create<BackgroundJob>()
+    .WithIdentity(name: "BackgroundJob", group: "JobGroup")
+    //_____ send value to execution method _____
+    .UsingJobData("OutPutMessage", "Dill Dill Pakistan Jaan jan Pakistan")
+    .UsingJobData("isActive", true)
+    .Build();
+
+DateTime startDate = new DateTime(2024, 3, 26, 9, 0, 0); // Set your desired start date and time
+
+var trigger = TriggerBuilder.Create()
+    .WithIdentity(name: "RepeatingTrigger", group: "TriggerGroup")
+    .StartAt(startDate) // Set the start date and time
+    .WithSimpleSchedule(s =>
+        s.WithIntervalInMinutes(20)
+         .RepeatForever()
+    )
+    .Build();
+
+var schedulerFactory = SchedulerBuilder.Create().Build();
+var scheduler = await schedulerFactory.GetScheduler();
+await scheduler.Start();
+
+await scheduler.ScheduleJob(job, trigger);
+Console.ReadKey();
+#endregion
